8c4ab0f5abd2a5d129efb012bf96620b437466d9
KIMGEONUNG <saywooong@gmail.com>
2020-07-04 11:38:11 +0900

    test: add BSplineCurveTest

--- SplineTest/BSplineCurveTest.cs
+++ SplineTest/BSplineCurveTest.cs
@@ -42,6 +42,7 @@ namespace SplineTest
             BSplineCurve bSpline = null;
             double[] r, a;
             double t;
+            double e;
 
             pts = new double[][]
                 {
@@ -58,6 +59,42 @@ namespace SplineTest
             r = bSpline.ParameterAt(t);
 
             Assert.IsTrue(a.SequenceEqual(r));
+
+
+            pts = new double[][]
+                {
+                    new double[]{ 0,0,0},
+                    new double[]{ 2,4,0},
+                    new double[]{ 4,0,0},
+                    new double[]{ 6,4,0},
+                };
+            knotset = new KnotSet(new double[] { 0, 0, 0, 0, 1, 1, 1, 1 });
+            degree = 3;
+            bSpline = new BSplineCurve(pts, knotset, degree);
+            t = 0.384;
+            a = new double[] { 2.3040000000000003, 1.9750256640000001, 0 };
+            r = bSpline.ParameterAt(t);
+            e = 0.0001;
+
+            Assert.IsTrue(a.SequenceEqual(r,e));
+
+
+            pts = new double[][]
+                {
+                    new double[]{ 0,0,0},
+                    new double[]{ 2,4,0},
+                    new double[]{ 4,0,0},
+                    new double[]{ 6,4,0},
+                };
+            knotset = new KnotSet(new double[] { 0, 0, 0, 0, 1, 1, 1, 1 });
+            degree = 3;
+            bSpline = new BSplineCurve(pts, knotset, degree);
+            t = 0.884;
+            a = new double[] { 5.304, 2.905969664, 0 };
+            r = bSpline.ParameterAt(t);
+            e = 0.0001;
+
+            Assert.IsTrue(a.SequenceEqual(r,e));
         }
     }
 }
