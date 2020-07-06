6a9fdd2e107c977a8ce3f36595efe22895172cb0
KIMGEONUNG <saywooong@gmail.com>
2020-07-06 14:34:46 +0900

    feat: class CurveDivider

--- Spline/Utils/CurveDivider.cs
+++ Spline/Utils/CurveDivider.cs
@@ -1,6 +1,8 @@
 ﻿using Spline.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Spline.Utils
@@ -9,11 +11,30 @@ namespace Spline.Utils
     {
         public static double[][] Divide(ParametricCurve crv, int divNum)
         {
-            throw new NotImplementedException();
+            List<double[]> rs = new List<double[]>();
+            double unit = 1 / (double)divNum;
+            
+            for (int i = 0; i <= divNum; i++)
+            {
+                double t = unit * i;
+                rs.Add(crv.ParameterAt(t));
+            }
+
+            return rs.ToArray();
         }
+
         public static double[][] GenerateSegments(ParametricCurve crv, int divNum)
         {
-            throw new NotImplementedException();
+            List<double[]> rs = new List<double[]>();
+            double[][] pts = Divide(crv, divNum);
+
+            for (int i = 0; i < divNum; i++)
+            {
+                double[] ln = pts[i].Concat(pts[i + 1]).ToArray();
+                rs.Add(ln);
+            }
+
+            return rs.ToArray();
         }
     }
 }
--- SplineTest/CurveDividerTest.cs
+++ SplineTest/CurveDividerTest.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Spline;
+using Spline.Utils;
+using SplineTest.Utils;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,13 +14,74 @@ namespace SplineTest
         [TestMethod]
         public void Divide()
         {
-            throw new Exception();
+            double[][] pts = null;
+            BezierCurve bezier = null;
+            int divNum;
+            double t;
+            double[][] r, a = null;
+            double e;
+
+            divNum = 4;
+            pts = new double[][]
+                {
+                    new double[]{ 0,0,0},
+                    new double[]{ 3,4,0},
+                    new double[]{ 6,0,0},
+                };
+            bezier = new BezierCurve(pts);
+            e = 0.001;
+
+            r = CurveDivider.Divide(bezier, divNum);
+            a = new double[][]
+                {
+                    new double[]{ 0,0,0},
+                    new double[] {1.5,1.5,0 },
+                    new double[] {3,2,0 },
+                    new double[] {4.5,1.5,0 },
+                    new double[] {6,0,0 },
+                };
+
+            Assert.IsTrue(a.Length == r.Length);
+            for (int i = 0; i < a.Length; i++)
+            {
+                Assert.IsTrue(a[i].SequenceEqual(r[i], e));
+            }
         }
 
         [TestMethod]
         public void GenerateSegments()
         {
-            throw new Exception();
+            double[][] pts = null;
+            BezierCurve bezier = null;
+            int divNum;
+            double t;
+            double[][] r, a = null;
+            double e;
+
+            divNum = 4;
+            pts = new double[][]
+                {
+                    new double[]{ 0,0,0},
+                    new double[]{ 3,4,0},
+                    new double[]{ 6,0,0},
+                };
+            bezier = new BezierCurve(pts);
+            e = 0.001;
+
+            r = CurveDivider.GenerateSegments(bezier, divNum);
+            a = new double[][]
+                {
+                    new double[]{ 0,0,0,1.5,1.5,0 },
+                    new double[] {1.5,1.5,0 ,3,2,0 },
+                    new double[] {3,2,0 ,4.5,1.5,0 },
+                    new double[] {4.5,1.5,0 ,6,0,0 },
+                };
+
+            Assert.IsTrue(a.Length == r.Length);
+            for (int i = 0; i < a.Length; i++)
+            {
+                Assert.IsTrue(a[i].SequenceEqual(r[i], e));
+            }
         }
     }
 }
